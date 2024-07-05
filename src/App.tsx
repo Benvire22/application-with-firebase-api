63e7a44535208096b261236fce96eda8891c279b
Limarev Ivan <ivantaketo@gmail.com>
2024-07-05 10:55:23 +0600

    Create Admin page, config axios request on firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
 import './App.css';
-import {Route, Routes} from 'react-router-dom';
+import {Link, Route, Routes} from 'react-router-dom';
 import Content from './containers/Content/Content';
+import AdminPage from './containers/AdminPage/AdminPage';
 
 const App = () => {
   return (
     <>
-     <Routes>
-       <Route path="/pages/:pageName" element={<Content />} />
-     </Routes>
+      <div>
+        <ul>
+          <li><Link to="/pages/home">Home</Link></li>
+          <li><Link to="/pages/about">About</Link></li>
+          <li><Link to="/pages/service">Our service</Link></li>
+          <li><Link to="/pages/contacts">Contacts</Link></li>
+          <li><Link to="/pages/policy">Policy</Link></li>
+          <li><Link to="/pages/admin">Admin Page</Link></li>
+        </ul>
+      </div>
+      <main className="container-xl">
+        <Routes>
+          <Route path="/" element={<Content/>}/>
+          <Route path="pages/:pageName" element={<Content/>}/>
+          <Route path="pages/admin" element={<AdminPage/>}/>
+          <Route path="*" element={<h1 className="text-center text-danger">Sorry, this page is not a found!</h1>} />
+        </Routes>
+      </main>
     </>
   );
 };
--- src/containers/Content/Content.tsx
+++ src/containers/Content/Content.tsx
@@ -1,12 +1,53 @@
-import {useParams} from 'react-router-dom';
+import {useNavigate, useParams} from 'react-router-dom';
+import {useCallback, useEffect, useState} from 'react';
+import axiosApi from '../../axiosApi';
+import {Page} from '../../types';
+import Spinner from '../../components/Spinner/Spinner';
 
 const Content = () => {
+  const [pageContent, setPageContent] = useState<Page | null>(null);
+  const [pageLoading, setPageLoading] = useState<boolean>(false);
+  const [isError, setIsError] = useState<boolean>(false);
   const {pageName} = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!pageName) {
+      navigate('pages/home');
+    }
+  }, [pageName]);
+
+  const pagesRequest = useCallback(async () => {
+    try {
+      setPageLoading(true);
+      const {data} = await axiosApi.get<Page>(`/${pageName}.json`);
+      console.log(data);
+
+      setPageContent(data);
+    } catch (e) {
+      setIsError(true);
+    } finally {
+      setPageLoading(false);
+    }
+  }, [pageName]);
+
+  useEffect(() => {
+    void pagesRequest();
+  }, [pagesRequest]);
 
   return (
-    <div>
-      <h1>{pageName || 'Home'}</h1>
-    </div>
+    <>
+      {pageLoading && <Spinner />}
+      {isError ? (
+        <h1 className="text-center text-danger">Sorry, unexpected Error was occurred!</h1>
+      ) : (
+        pageContent ? (
+          <div>
+            <h1>{pageContent.title}</h1>
+            <p>{pageContent.content}</p>
+          </div>
+        ) : <h1 className="text-center text-danger">Sorry, this page is not a found!</h1>)}
+    </>
   );
 };
 
